97027bf30eb23492e4d7994d4e01aad1fef8c37c
dentvin <danial00336537834@gmail.com>
2025-10-10 13:29:09 +0300

    Добавлено консольное приложение TodoList

---
+++ TodoList/TodoList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Работу выполнили Сербин Данил еееее");
+
+        // Запрос данных пользователя
+        Console.Write("Введите имя: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введите фамилию: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Введите год рождения: ");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        // Рассчёт возраста
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {age}");
+    }
+}
